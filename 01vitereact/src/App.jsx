ac11c93738ddbd53f9045ca98692b88498febbe6
Rajeev Tiwari <rajeevtiwari30305@gmail.com>
2024-08-16 21:35:22 +0530

    Got to know how does react Library works

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Hello from "./hello";
 
 function App() {
+  const username = "Rajeev" 
+  
+  //{any thing in this curly braces is known as a evaluated expression}  or will give the final ourcome
   return (
     <>
+
     <Hello></Hello>
-    <h1>Creating react from vite</h1>
+    <h1>Creating react from vite and it is made by {username}</h1>
     </>     
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App"
 
+function MyApp(){
+  return (
+      <div>
+          <h1>Hello World from vite React</h1>
+      </div>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+
+
+const anotherUser = "understan ding react working"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com',target: '_blank' },
+  'click me to visit google',
+  anotherElement
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App></App>
-    </React.StrictMode>
+    <App/>
 )
